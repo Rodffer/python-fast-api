749b1f255dc80750fb204474fb61624ddff736eb
Rayron Rodffer <r.rodffer@gmail.com>
2022-02-02 16:35:08 -0300

    Feat: adicionado rota de update e exceções

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from typing import List
-from uuid import UUID, uuid4
-from models import User, Gender, Role
+from uuid import UUID
+from models import User, Gender, Role, UserUpdateRequest
 
 app = FastAPI()
 
@@ -40,4 +40,26 @@ async def delete_user(user_id: UUID):
   for user in db:
     if user.id == user_id:
       db.remove(user)
-      return
+      return {"user has been removed"}
+  raise HTTPException(
+    status_code=404,
+    detail=f"user with id: {user_id} does not exists"
+  )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+  for user in db:
+    if user.id == user_id:
+      if user_update.first_name is not None:
+        user.first_name = user_update.first_name
+      if user_update.last_name is not None:
+        user.last_name = user_update.last_name
+      if user_update.middle_name is not None:
+        user.middle_name = user_update.middle_name
+      if user_update.roles is not None:
+        user.roles = user_update.roles
+      return {"user has been updated"}
+  raise HTTPException(
+    status_code=404,
+    detail=f"user with id: {user_id} does not exists"
+  )
